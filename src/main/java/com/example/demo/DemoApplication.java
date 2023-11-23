aeb10f23bdb8f6b29d1e68a167ef99250722c7df
Marcelino Maita Vargas <maita.marcelino@utec.edu.pe>
2023-11-22 20:11:13 -0500

    tercero_c

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.demo.domain.entities.Autor;
 import com.example.demo.domain.entities.Comic;
